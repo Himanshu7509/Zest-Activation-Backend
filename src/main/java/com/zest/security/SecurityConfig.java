dfd6daf24a12094978547b58bdef1eb9153bc756
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-12 12:14:11 +0530

    FRONT END PORT

--- src/main/java/com/zest/security/SecurityConfig.java
+++ src/main/java/com/zest/security/SecurityConfig.java
@@ -45,7 +45,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("https://event-pro-plus.vercel.app/"));
+        
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
