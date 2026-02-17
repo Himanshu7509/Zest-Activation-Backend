1c1cb8b30fd9c013627eafbd217f228e763f63b4
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-17 11:32:29 +0530

    SECURITY

--- src/main/java/com/zest/controller/AdminController.java
+++ src/main/java/com/zest/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final UserRepository userRepository;
