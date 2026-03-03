2208f051cc3d4f3b4f2d58077bb824ac281bba11
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-03-04 05:19:06 +0530

    UPDATE

--- src/main/java/com/zest/ZestApplication.java
+++ src/main/java/com/zest/ZestApplication.java
@@ -2,8 +2,10 @@ package com.zest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ZestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/zest/controller/ProfileController.java
+++ src/main/java/com/zest/controller/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +47,7 @@ public class ProfileController {
     @PutMapping
     public ResponseEntity<ProfileResponse> updateProfile(
             Authentication authentication,
-            ProfileUpdateRequest request) {
+            @RequestBody ProfileUpdateRequest request) {
         
         String userId = authentication.getName();
         log.info("Updating profile for user: {}", userId);
