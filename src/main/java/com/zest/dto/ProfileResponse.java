880cf9196a6cacd3c57c8e42327f011fc2917469
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-03-05 11:42:47 +0530

    DTO

--- src/main/java/com/zest/dto/ProfileResponse.java
+++ src/main/java/com/zest/dto/ProfileResponse.java
@@ -1,7 +1,5 @@
 package com.zest.dto;
 
-import java.time.LocalDateTime;
-
 import com.zest.model.Role;
 
 import lombok.Data;
@@ -18,6 +16,6 @@ public class ProfileResponse {
     private String location;
     private Boolean isActive;
     private Boolean approved;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+   // private LocalDateTime createdAt;
+   // private LocalDateTime updatedAt;
 }
